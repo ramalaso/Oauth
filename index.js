e2aa40934bf0692e07bae4b7c5ac1330acccb54c
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-05 14:13:06 -0400

    Added Google Oauth Authentication

--- index.js
+++ index.js
@@ -19,18 +19,29 @@ app.use(cookieSession({
     keys: ['key1', 'key2']
 }));
 
+const isLoggedIn = (req, res, next) => {
+    if (req.user) next();
+    else res.sendStatus(401);
+};
+
+app.get('/', (req, res) => res.send('You are not logged in'));
 app.get('/failed', (req, res) => re.send('You are failed to login'));
-app.get('/good', (req, res) => re.send('Welcome ' + req.user.email));
+app.get('/good', isLoggedIn, (req, res) => re.send('Welcome ' + req.user.email));
 
-app.get('/google',
-    passport.authenticate('google', { scope: ['profile'] }));
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/google/callback',
+app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/failed' }),
     function (req, res) {
         // Successful authentication, redirect home.
         res.redirect('/good');
     });
+
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
--- passport-setup.js
+++ passport-setup.js
@@ -3,14 +3,22 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 require('dotenv').config();
 
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(err, user);
+});
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback"
+    callbackURL: "http://localhost:3000/auth/google/callback"
 },
     function (accessToken, refreshToken, profile, done) {
-        User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            return done(err, user);
-        });
+        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(null, profile);
+        // });
     }
 ));
